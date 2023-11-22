ec129f1635fb847b09b827ebff46c3c801df268b
filippo <riccardo.gabba02@gmail.com>
2023-11-22 16:55:16 +0100

    finished the basement of the header

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
-  <Header/>
+  
+  <div class="bg-header">
+    <Header />
+  </div>
+
 </template>
 
 <script>
-import Header from "./components/Header.vue"
-  export default {
-    name: 'app',
-    
-    components: {
-      Header,
-    }
-  }
+import Header from "./components/Header.vue";
+export default {
+  name: "app",
+
+  components: {
+    Header,
+  },
+};
 </script>
 
-
-
-
-
-
-
-
-
-
-
-
 <style lang="scss" scoped>
-
+  .bg-header {
+    background-image: url(../public/Images/header-image-homepage.jpg);
+    background-size: 100%;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,70 @@
 <template>
-   <div>
-    <h1>ciao</h1>
-   </div>
+  <header class="text-white">
+    <div class="d-flex justify-content-between p-4">
+      <div>
+        <img src="../../public/Images/medical_logo_1x_light.png" alt="" />
+      </div>
+      <div class="d-flex align-items-center">
+        <h3 class="mx-3">Home</h3>
+        <h3 class="mx-3">About</h3>
+        <h3 class="mx-3">Departments</h3>
+        <h3 class="mx-3">Articles</h3>
+        <h3 class="p-3 bg-primary">Make Appointment</h3>
+      </div>
+    </div>
+
+    <div class="container justify-content-center align-items-center">
+      <div class="d-flex">
+        <div class="col-6">
+          <h1>Caring For Life</h1>
+          <p>
+            Kind words can be short and easy to speak, but their enchoes <br />
+            are truly endless. Avada Health focuses on yo as if it was our own
+            family
+          </p>
+          <button class="btn btn-primary" type="submit">Learn More</button>
+        </div>
+        <div class="col-3">
+          <h3 class="text-center">Office Hours</h3>
+          <hr />
+
+          <table class="w-100">
+            <tbody>
+              <tr>
+                <td>
+                  <span>Monday - Friday</span>
+                </td>
+                <td>
+                  <span>9.00 -- 2.00</span>
+                </td>
+              </tr>
+              <tr>
+                <td>Jacob</td>
+                <td>8:00 - 5:00</td>
+              </tr>
+              <tr>
+                <td>Sunday</td>
+                <td>8:00 - 5:00</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="my-3">
+            <h3>+44(0) 1865 339665</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name:"Header",
+  name: "Header",
 
-
-data(){
-    return {
-
-    }
-}
-}
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
